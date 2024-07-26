2fe4c80a4bc9894337ab9f3efb39de6672e01c57
shanmukha <nath.shanmukha@gmail.com>
2024-07-26 22:42:52 +0530

    separated workflows

--- ecs.tf
+++ ecs.tf
@@ -30,6 +30,33 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   ]
 }
 
+resource "aws_iam_policy" "dynamodb_access_policy" {
+  name = "${var.project_name}-dynamodb-access-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:DescribeTable",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem"
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/MyNoSQLTable"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.dynamodb_access_policy.arn
+}
+
 resource "aws_ecs_task_definition" "frontend" {
   family                   = "${var.project_name}-frontend"
   network_mode             = "awsvpc"
@@ -38,6 +65,7 @@ resource "aws_ecs_task_definition" "frontend" {
   memory                   = "512"
 
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -48,6 +76,16 @@ resource "aws_ecs_task_definition" "frontend" {
         {
           containerPort = 80
           hostPort      = 80
+        }
+      ]
+      environment = [
+        {
+          name  = "AWS_REGION"
+          value = "us-east-1"
+        },
+        {
+          name  = "DYNAMODB_TABLE"
+          value = "MyNoSQLTable"
         }
       ]
     }
@@ -62,6 +100,7 @@ resource "aws_ecs_task_definition" "backend" {
   memory                   = "512"
 
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -70,8 +109,18 @@ resource "aws_ecs_task_definition" "backend" {
       essential = true
       portMappings = [
         {
-          containerPort = 80
-          hostPort      = 80
+          containerPort = 5000
+          hostPort      = 5000
+        }
+      ]
+      environment = [
+        {
+          name  = "AWS_REGION"
+          value = "us-east-1"
+        },
+        {
+          name  = "DYNAMODB_TABLE"
+          value = "MyNoSQLTable"
         }
       ]
     }
@@ -118,7 +167,7 @@ resource "aws_ecs_service" "backend" {
   load_balancer {
     target_group_arn = aws_lb_target_group.backend.arn
     container_name   = "backend"
-    container_port   = 80
+    container_port   = 5000
   }
 
   depends_on = [aws_lb_listener.backend]
@@ -134,7 +183,7 @@ resource "aws_lb_target_group" "frontend" {
 
 resource "aws_lb_target_group" "backend" {
   name        = "${var.project_name}-backend-tg"
-  port        = 80
+  port        = 5000
   protocol    = "TCP"
   vpc_id      = aws_vpc.app_vpc.id
   target_type = "ip"
@@ -153,7 +202,7 @@ resource "aws_lb_listener" "frontend" {
 
 resource "aws_lb_listener" "backend" {
   load_balancer_arn = aws_lb.nlb.arn
-  port              = 8080
+  port              = 5000
   protocol          = "TCP"
 
   default_action {
---
+++ iam.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "dynamodb_access_policy" {
+  name = "${var.project_name}-dynamodb-access-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:DescribeTable",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem"
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/MyNoSQLTable"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.dynamodb_access_policy.arn
+}
