3748767131af3c14c938f58394421fb169c1180d
shanmukha <nath.shanmukha@gmail.com>
2024-07-26 22:51:35 +0530

    removed db ecr

--- ecr.tf
+++ ecr.tf
@@ -6,18 +6,10 @@ resource "aws_ecr_repository" "backend" {
   name = "${var.project_name}-backend"
 }
 
-resource "aws_ecr_repository" "database" {
-  name = "${var.project_name}-database"
-}
-
 output "frontend_ecr_repository_url" {
   value = aws_ecr_repository.frontend.repository_url
 }
 
 output "backend_ecr_repository_url" {
   value = aws_ecr_repository.backend.repository_url
-}
-
-output "database_ecr_repository_url" {
-  value = aws_ecr_repository.database.repository_url
 }
