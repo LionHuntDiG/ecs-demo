10be0e1cb6cd56e8deea69d24821cb78202a7cca
shanmukha <nath.shanmukha@gmail.com>
2024-07-27 12:11:10 +0530

    fmt

--- ecr.tf
+++ ecr.tf
@@ -1,10 +1,10 @@
 resource "aws_ecr_repository" "frontend" {
-  name = "${var.project_name}-frontend"
+  name         = "${var.project_name}-frontend"
   force_delete = true
 }
 
 resource "aws_ecr_repository" "backend" {
-  name = "${var.project_name}-backend"
+  name         = "${var.project_name}-backend"
   force_delete = true
 }
 
