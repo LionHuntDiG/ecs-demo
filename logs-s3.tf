f10dd28dc6d16940fafd8c33efdbc2c270d116ec
shanmukha <nath.shanmukha@gmail.com>
2024-07-27 21:39:56 +0530

    fix

--- logs-s3.tf
+++ logs-s3.tf
@@ -20,17 +20,9 @@ resource "aws_s3_bucket_policy" "alb_logs_policy" {
             "aws:SourceAccount" = "462585606803" # Replace with your AWS Account ID
           },
           ArnLike = {
-            "aws:SourceArn" = "arn:aws:elasticloadbalancing:us-east-1:462585606803:loadbalancer/app/myapp-alb/*"
+            "aws:SourceArn" = "arn:aws:elasticloadbalancing:us-east-1:462585606803:loadbalancer/*"
           }
         }
-      },
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action = "s3:PutObject",
-        Resource = "${aws_s3_bucket.alb_logs.arn}/*"
       }
     ]
   })
