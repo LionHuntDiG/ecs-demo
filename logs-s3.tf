e3db5518dc81b42e9a41eb9d642563806ee17046
shanmukha <nath.shanmukha@gmail.com>
2024-07-27 21:30:19 +0530

    fix

--- logs-s3.tf
+++ logs-s3.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "alb_logs" {
   bucket = "${var.project_name}-alb-logs"
 }
 
-resource "aws_s3_bucket_acl" "alb_logs_acl" {
-  bucket = aws_s3_bucket.alb_logs.bucket
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_policy" "alb_logs_policy" {
   bucket = aws_s3_bucket.alb_logs.bucket
 
@@ -14,12 +9,12 @@ resource "aws_s3_bucket_policy" "alb_logs_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "AWSLogDeliveryWrite"
-        Effect    = "Allow"
+        Sid       = "AWSLogDeliveryWrite",
+        Effect    = "Allow",
         Principal = {
           Service = "logdelivery.elasticloadbalancing.amazonaws.com"
-        }
-        Action = "s3:PutObject"
+        },
+        Action = "s3:PutObject",
         Resource = "${aws_s3_bucket.alb_logs.arn}/*"
       }
     ]
