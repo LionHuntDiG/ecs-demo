a6e018907c1455d67105bec2c4ef45bc4f9e0802
shanmukha <nath.shanmukha@gmail.com>
2024-07-27 21:46:42 +0530

    fix

--- logs-s3.tf
+++ logs-s3.tf
@@ -6,24 +6,17 @@ resource "aws_s3_bucket_policy" "alb_logs_policy" {
   bucket = aws_s3_bucket.alb_logs.bucket
 
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    Version: "2012-10-17",
+    Statement: [
       {
-        Effect = "Allow",
-        Principal = {
-          Service = "elasticloadbalancing.amazonaws.com"
+        Effect: "Allow",
+        Principal: {
+          Service: "elasticloadbalancing.amazonaws.com"
         },
-        Action = "s3:PutObject",
-        Resource = "${aws_s3_bucket.alb_logs.arn}/*",
-        Condition = {
-          StringEquals = {
-            "aws:SourceAccount" = "462585606803" # Ensure this is your AWS Account ID
-          },
-          ArnLike = {
-            "aws:SourceArn" = "arn:aws:elasticloadbalancing:us-east-1:462585606803:loadbalancer/app/myapp-alb/703224e881eac8b4"
-          }
-        }
+        Action: "s3:*",
+        Resource: "*"
       }
     ]
   })
 }
+
